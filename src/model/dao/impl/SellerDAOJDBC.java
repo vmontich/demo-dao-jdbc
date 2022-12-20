c50ea65847e9f330f09534e76af42fab0642f551
Vitor Montich <vmontich@hotmail.com>
2022-12-19 22:11:47 -0300

    SellerDAOJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+public class SellerDAOJDBC implements SellerDAO {
+
+	@Override
+	public void insert(Seller department) {
+		
+	}
+
+	@Override
+	public void update(Seller department) {
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+}
